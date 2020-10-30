7847ea297ab690bf1d4188089658e2e96ca0415e
CaveDann <74264942+CaveDann@users.noreply.github.com>
2020-10-30 09:15:31 +0000

    Update Structure

--- media/lua/client/ISUI/AKCUpdate.lua
+++ media/lua/client/ISUI/AKCUpdate.lua
@@ -2,8 +2,8 @@ AKCUpdate = {}
 
 function AKCUpdate.updateText()
     local zText = "Zombies Killed (Total): <LINE> Zombies Killed (Weapons): <LINE> Zombies Killed (Other): ";
-    AKCTab.HomeWindow.text = zText
-    AKCTab.HomeWindow:paginate()
+    AKCTab.HomeWindow.text = zText;
+    AKCTab.HomeWindow:paginate();
 end
 
 Events.OnPlayerUpdate.Add(AKCUpdate.updateText);
