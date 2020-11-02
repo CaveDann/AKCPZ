330352df45489f2d2f5078b24a3a415e74adf3a3
CaveDann <74264942+CaveDann@users.noreply.github.com>
2020-11-02 19:06:38 +0000

    Make Counters Look Reasonable on Pre-Existing
    Counters now check the game kills on load, rather than after first
    kill in current session.

--- media/lua/client/ISUI/AKCStart.lua
+++ media/lua/client/ISUI/AKCStart.lua
@@ -1,7 +1,13 @@
 AKCStart = {}
 
+stk = 0;
+sgk = 0;
+sok = 0;
+
 function AKCStart.drawText()
-    local pText = "Zombies Killed (Total): "..tostring(0).." <LINE> Zombies Killed (Weapons): "..tostring(1).." <LINE> Zombies Killed (Other): "..tostring(2);
+    stk = getPlayer():getZombieKills();
+    sgk = getPlayer():getZombieKills();
+    local pText = "Zombies Killed (Total): "..tostring(stk).." <LINE> Zombies Killed (Weapons): "..tostring(sgk).." <LINE> Zombies Killed (Other): "..tostring(sok);
     AKCTab.HomeWindow.text = pText;
     AKCTab.HomeWindow:paginate();
 end
