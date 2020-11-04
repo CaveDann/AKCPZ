73973f59f76feb1ec7560670122b91eef99d4732
CaveDann <74264942+CaveDann@users.noreply.github.com>
2020-11-04 19:54:25 +0000

    Make Counters Read File on Game Start
    The AKCStart.lua now looks to read the AKCDataFile.txt on game start,
    so that the counters are persistent across saves.

--- media/lua/client/ISUI/AKCStart.lua
+++ media/lua/client/ISUI/AKCStart.lua
@@ -3,11 +3,43 @@ AKCStart = {}
 stk = 0;
 sgk = 0;
 sok = 0;
+file = "AKCDataFile.txt";
+mod = "AKCPZ";
 
 function AKCStart.drawText()
-    stk = getPlayer():getZombieKills();
-    sgk = getPlayer():getZombieKills();
+    if AKCFile.testData() then
+        klist = AKCFile.readData();
+        stk = klist[1];
+        sgk = klist[2];
+        sok = klist[3];
+        ok = sok;
+    else
+        stk = getPlayer():getZombieKills();
+        sgk = getPlayer():getZombieKills();
+        ok = 0;
+    end
+    tk = stk;
+    gk = sgk;
+    ngk = sgk;
+    ogk = sgk;
     local pText = "Zombies Killed (Total): "..tostring(stk).." <LINE> Zombies Killed (Weapons): "..tostring(sgk).." <LINE> Zombies Killed (Other): "..tostring(sok);
     AKCTab.HomeWindow.text = pText;
     AKCTab.HomeWindow:paginate();
+end
+
+function AKCFile.testData()
+    local f = getModFileReader(mod, file, false);
+    if f then f:close() end;
+    return f ~= nil;
+end
+
+function AKCFile.readData()
+    if not AKCFile.testData() then return {} end
+    lines = {}
+    local f = getModFileReader(mod, file, true)
+    for i = 1,3 do
+        line = f:readLine();
+        lines[#lines + 1] = line;
+    end
+    return lines
 end
